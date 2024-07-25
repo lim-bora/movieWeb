b052a7796f55e0819164d3afa0890f1b7ed4ff37
임보라 <imbora@imbolaui-MacBookAir.local>
2024-07-25 16:37:05 +0900

    모달창 추가

--- script/index.js
+++ script/index.js
@@ -76,5 +76,18 @@ nav.addEventListener("mouseout", () => {
 }); 
 
 
+const movieModal = document.querySelector('.movie-modal');
+const modalOpen = document.querySelector('.modal_btn');
+const modalClose = document.querySelector('.close_btn');
 
+//열기 버튼을 눌렀을 때 모달팝업이 열림
+modalOpen.addEventListener('click',function(){
+    //display 속성을 block로 변경
+    movieModal.style.display = 'block';
+});
+//닫기 버튼을 눌렀을 때 모달팝업이 닫힘
+modalClose.addEventListener('click',function(){
+   //display 속성을 none으로 변경
+   movieModal.style.display = 'none';
+});
 
--- script/tmdb-api.js
+++ script/tmdb-api.js
@@ -14,7 +14,7 @@ const options = {
 
         const movieList = document.querySelector('.movie-list'); //무비박스
         const popularList = document.querySelector('.popular-list'); //인기순ul
-        
+        const modalPopularList = document.querySelector('.modal-popular-list');
    
         data.results.forEach(e => { //data.results에 있는 모든 요소 e에 담기
             const moviesItemCreate = document.createElement('li'); //movies classname을 가진 li 만듬
@@ -27,16 +27,30 @@ const options = {
                 <h2>${e.title}</h2>
             `;
             popularList.appendChild(moviesItemCreate); //인기순ul안에 li 아이템들 추가
+            
 
+            const modalItemCreate = document.createElement('li');
+            ////modal li요소 안에 넣을 html 태그들 : 백틱으로 묶기
+            modalItemCreate.innerHTML = ` 
+                <img class="moviesImg" src="https://image.tmdb.org/t/p/w500${e.poster_path}" alt="${e.title}">
+                <h2>${e.title}</h2>
+            `;
+            modalPopularList.appendChild(modalItemCreate); //인기순ul안에 li 아이템들 추가
+
+
+
+
+            ////클릭시 alert창 띄우기
             const moviesItem = document.querySelectorAll('.moviesItem');
-
-            moviesItem.forEach((item) => {
-                item.addEventListener("click", () => {
-                    alert(`영화 id: ${item.getAttribute('data-id')}`);
+            for (let i = 0; i < moviesItem.length; i++) {
+                moviesItem[i].addEventListener("click", () => {
+                    alert(`영화 id: ${moviesItem[i].getAttribute('data-id')}`);
                 }); 
-            });
+            }
+
         });
         movieList.appendChild(popularList); //movieList안에 popularList-ul 추가
+        // movieList.appendChild(popularList); //movieList안에 popularList-ul 추가
         
     })
     .catch(err => console.error(err));
